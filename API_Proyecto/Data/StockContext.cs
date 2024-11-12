294c840e7a11409eb8cb179a38a8d80ac52d52ba
Mateo Mista <mateomista5@gmail.com>
2024-11-12 20:31:56 -0300

    implement db

---
+++ API_Proyecto/Data/StockContext.cs
@@ -0,0 +1,28 @@
+﻿using API_Proyecto.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Proyecto.Data
+{
+    public class StockContext : DbContext
+    {
+        public StockContext(DbContextOptions<StockContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<API_Proyecto.Models.Producto>? Productos { get; set; }
+        public DbSet<API_Proyecto.Models.Posicion>? Posiciones { get; set; }
+        public DbSet<API_Proyecto.Models.Cliente>? Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>()
+                .HasKey(c => c.CUIT);
+            modelBuilder.Entity<Producto>()
+                .HasKey(c => c.IdProducto);
+            modelBuilder.Entity<Posicion>()
+                .HasKey(c => c.Id);
+            
+        }
+    }
+}
---
+++ API_Proyecto/Migrations/20241112224804_ClienteAtributes.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Proyecto.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClienteAtributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClienteCUIT",
+                table: "Posiciones",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Apynom",
+                table: "Clientes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RazonSocial",
+                table: "Clientes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posiciones_ClienteCUIT",
+                table: "Posiciones",
+                column: "ClienteCUIT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posiciones_Clientes_ClienteCUIT",
+                table: "Posiciones",
+                column: "ClienteCUIT",
+                principalTable: "Clientes",
+                principalColumn: "CUIT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posiciones_Clientes_ClienteCUIT",
+                table: "Posiciones");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posiciones_ClienteCUIT",
+                table: "Posiciones");
+
+            migrationBuilder.DropColumn(
+                name: "ClienteCUIT",
+                table: "Posiciones");
+
+            migrationBuilder.DropColumn(
+                name: "Apynom",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "RazonSocial",
+                table: "Clientes");
+        }
+    }
+}
